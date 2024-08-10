a8aef55dd90a56b32bceaa745003e7c9faefa660
Gr1ll <cyril.kurmann@gmail.com>
2024-08-10 18:52:24 +0530

    fix: pr

--- apps/frontend/src/app/shared/feedback/error/error.component.ts
+++ apps/frontend/src/app/shared/feedback/error/error.component.ts
@@ -15,7 +15,6 @@ export class ErrorComponent {
   @Input() action = 'Try again'
   @Input() route = '/'
   @Input() callback: (() => void) | null = null
-  @Input() showAction = true
   @Output() actionClicked = new EventEmitter<void>()
 
   constructor(private router: Router) {}
