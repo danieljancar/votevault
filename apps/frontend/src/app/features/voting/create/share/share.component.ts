00b52726539b0acc608682b34cadc72767f55e8f
Cyril Kurmann <cyril.kurmann@gmail.com>
2024-08-11 13:47:36 +0530

    feat: add download button to create vote (#44)
    * feat: add download button to create vote
    
    * fix: change share component download text
    
    ---------
    
    Co-authored-by: Daniel Jancar <danieljancar@bluewin.ch>

--- apps/frontend/src/app/features/voting/create/share/share.component.ts
+++ apps/frontend/src/app/features/voting/create/share/share.component.ts
@@ -11,9 +11,21 @@ import { FormsModule } from '@angular/forms'
 })
 export class ShareComponent {
   @Input({ required: true }) voteId = ''
+  @Input({ required: true }) voteTitle = ''
 
   public copyLinkToClipboard(voteId: string): void {
     const url = 'https://vv.danieljancar.dev/voting/' + voteId
     navigator.clipboard.writeText(url)
+  }
+
+  downloadVoteID() {
+    const voteIdData = `Vote ID: ${this.voteId}`
+    const blob = new Blob([voteIdData], { type: 'text/plain;charset=utf-8' })
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = `${this.voteTitle}_vote_id.txt`
+    a.click()
+    window.URL.revokeObjectURL(url)
   }
 }
