76554e6b4c4ead25db8b3614fe40203764d934ad
Cyril Kurmann <cyril.kurmann@gmail.com>
2024-08-16 01:31:39 +0530

    feat: add footer to the bottom of the page (#80)

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { NavbarComponent } from './shared/navigation/navbar/navbar.component'
 import { DisclaimerComponent } from './shared/navigation/disclaimer/disclaimer.component'
+import { FooterComponent } from './shared/footer/footer.component'
 
 @Component({
   standalone: true,
-  imports: [RouterModule, NavbarComponent, DisclaimerComponent],
+  imports: [
+    RouterModule,
+    NavbarComponent,
+    DisclaimerComponent,
+    FooterComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
